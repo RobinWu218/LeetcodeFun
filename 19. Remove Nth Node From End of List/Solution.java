2a686dbd3586fdd6cc326dbae14f9042143e4c87
apple <apple@appledeiMac.local>
2017-07-16 22:07:35 +0800

    add 19

---
+++ 19. Remove Nth Node From End of List/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        ListNode fast = dummy;
+        while (n>0){
+            fast = fast.next;
+            n--;
+        }
+        while (fast.next != null){
+            fast = fast.next;
+            cur = cur.next;
+        }
+        cur.next = cur.next.next;
+        return dummy.next;
+    }
+}
